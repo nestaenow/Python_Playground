a71dd7da87192d5769e7b53bc2588183b129dd7d
nestaenow <nestaenow@gmail.com>
2022-07-19 23:36:54 +0100

    progress

--- strings.py
+++ strings.py
@@ -3,3 +3,15 @@ second_word = 'There!'
 
 #Using the + operator to concatinate two strings
 print (first_word + ' ' + second_word)
+
+#To print a string several times 
+print (first_word * 4)
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print(username + ' accessed the site ' + url + ' at ' + timestamp + '.')
