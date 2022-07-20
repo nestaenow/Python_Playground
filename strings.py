cf95832aaec910a43494a08b66107c6ccc432911
nestaenow <nestaenow@gmail.com>
2022-07-20 13:26:13 +0100

    progress

--- strings.py
+++ strings.py
@@ -15,3 +15,15 @@ url = "http://petshop.com/pets/mammals/cats"
 # The message should have the same format as this one:
 # "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
 print(username + ' accessed the site ' + url + ' at ' + timestamp + '.')
+
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+#todo: calculate how long this name is
+name_length = len(given_name + middle_names + family_name) + 2
+#2 accounts for the two spaces between the names
+
+# Now we check to make sure that the name fits within the driving license character limit
+# Nothing you need to do here
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
