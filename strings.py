6c86cfd5ff96a2faf048202dbc3bf54a176d4477
nestaenow <nestaenow@gmail.com>
2022-07-20 17:29:31 +0100

    Type and type conversion

--- strings.py
+++ strings.py
@@ -27,3 +27,16 @@ name_length = len(given_name + middle_names + family_name) + 2
 # Nothing you need to do here
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+total_sales = int(mon_sales)+int(tues_sales) + \
+    int(wed_sales)+int(thurs_sales)+int(fri_sales)
+total_sales = str(total_sales)
+print("This week's total sales: " + total_sales)
