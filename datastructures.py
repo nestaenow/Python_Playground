53ddccd08bc443078c016ade89b14d093066cda9
nestaenow <nestaenow@gmail.com>
2022-07-22 00:17:24 +0100

    lists and memberships

--- datastructures.py
+++ datastructures.py
@@ -15,3 +15,23 @@ print(second_half)
 
 # Using the membership operators 'in' and 'not in'
 print('Sunday' in months, 'Sunday' not in months)
+
+# List Indexing
+month = 8
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# use list indexing to determine the number of days in month
+num_days = days_in_month[month - 1]
+
+print(num_days)
+
+# Slicing Lists
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+
+# TODO: Modify this line so it prints the last three elements of the list
+eclipse_dates = eclipse_dates[-3:]
+# This could equally work --> eclipse_dates = eclipse_dates[-3:len(eclipse_dates)
+print(eclipse_dates)
