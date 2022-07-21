55db5a2fca33478b697f97257f5459d310313702
nestaenow <nestaenow@gmail.com>
2022-07-21 23:40:34 +0100

    list datatype

---
+++ datastructures.py
@@ -0,0 +1,15 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+
+q3 = months[6:9]
+# [ 'July', 'August', 'September']
+print(q3)  
+
+first_half = months[:6]
+# ['January', 'February', 'March', 'April', 'May', 'June']
+print(first_half)  
+
+second_half = months[6:]
+# ['July', 'August', 'September', 'October', 'November', 'December']
+print(second_half)
+
