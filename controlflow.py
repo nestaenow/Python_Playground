477692714bad97bf41f7244b47e2f904a2c7376f
nestaenow <nestaenow@gmail.com>
2022-07-29 19:53:46 +0100

    if elif else statement

--- controlflow.py
+++ controlflow.py
@@ -8,3 +8,17 @@ else:
     print('No call made!')
 
 print(phone_balance, bank_balance)
+
+# if, elif and else statements
+season = 'summer'
+
+if season == 'spring':
+    print('plant the garden!')
+elif season == 'summer':
+    print('water the garden!')
+elif season == 'fall':
+    print('harvest the garden!')
+elif season == 'winter':
+    print('stay indoors!')
+else:
+    print('unrecognized season')
