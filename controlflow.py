9c56a13d4d417bd124c4af17c8c2ad1dc9ae00a6
nestaenow <nestaenow@gmail.com>
2022-07-29 20:57:06 +0100

    guess game

--- controlflow.py
+++ controlflow.py
@@ -69,3 +69,26 @@ else:
 message = "Somebody who is {} years old will pay ${} to ride the bus.".format(
     age, ticket_price)
 print(message)
+
+
+# Test 1
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 10
+guess = 30
+
+if guess < answer:
+    result = "Oops!  Your guess was too low."
+elif guess > answer:
+    result = "Oops!  Your guess was too high."
+elif guess == answer:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
