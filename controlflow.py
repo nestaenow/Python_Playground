ccb0632cf00f55d54591a890252a1f77ba5202dc
nestaenow <nestaenow@gmail.com>
2022-08-06 14:43:39 +0100

    Break the string

--- controlflow.py
+++ controlflow.py
@@ -382,3 +382,21 @@ for cargo_name, cargo_weight in manifest:
 
 print("\nFinal Weight: {}".format(weight))
 print("Final Items: {}".format(items))
+
+# Test I
+# Break the string
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+for headline in headlines:
+    news_ticker += headline + " "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:140]
+        break
+
+print(news_ticker)
