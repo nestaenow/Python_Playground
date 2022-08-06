0b79efa44f16c10b0ab96da325ee670df402f873
nestaenow <nestaenow@gmail.com>
2022-08-06 16:19:10 +0100

    ZIP & ENUMERATE

--- controlflow.py
+++ controlflow.py
@@ -432,3 +432,53 @@ for letter, num in zip(letters, nums):
 letters = ['a', 'b', 'c', 'd', 'e']
 for i, letter in enumerate(letters):
     print(i, letter)
+
+# Test I
+# Zip Coordinates
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+for point in zip(labels, x_coord, y_coord, z_coord):
+    # Notice here, the tuple was unpacked using * in the format method making the code cleaner.
+    points.append("{}: {}, {}, {}".format(*point))
+
+for point in points:
+    print(point)
+
+# Test II
+# Zip Lists to a Dictionary
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+# Test III
+# Unzip Tuples
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72),
+        ("Lily", 66), ("Marshall", 76))
+
+names, heights = zip(*cast)
+print(names)
+print(heights)
+
+# Test IV
+# Transpose with Zip
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+
+# Test V
+# Enumerate
+cast = ["Barney Stinson", "Robin Scherbatsky",
+        "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for i, character in enumerate(cast):
+    cast[i] = character + " " + str(heights[i])
+
+print(cast)
