b3058a59543826c385428077b18577e048eaa666
nestaenow <nestaenow@gmail.com>
2022-08-06 12:53:19 +0100

    sum of the first five od numbers in the list

--- controlflow.py
+++ controlflow.py
@@ -322,12 +322,20 @@ print(nearest_square)
 print(squares)
 
 # Test III
-# Sum of odd numbers found in the first five indexes
+# Sum of the first five odd numbers found in the list
 num_list = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542,
             87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
-sum_of_odds = 0
 
-for num in range(5):
-    if num_list[num] % 2 != 0:
-        sum_of_odds += num_list[num]
-print(sum_of_odds)
+count_odd = 0
+list_sum = 0
+i = 0
+len_num_list = len(num_list)
+
+while (count_odd < 5) and (i < len_num_list):
+    if num_list[i] % 2 != 0:
+        list_sum += num_list[i]
+        count_odd += 1
+    i += 1
+
+print("The numbers of odd numbers added are: {}".format(count_odd))
+print("The sum of the odd numbers added is: {}".format(list_sum))
