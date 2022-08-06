5a72ba4602d4c6b536bae0d0e3faf369c4503ae9
nestaenow <nestaenow@gmail.com>
2022-08-06 12:15:29 +0100

    while loops

--- controlflow.py
+++ controlflow.py
@@ -277,3 +277,45 @@ while current <= number:
 # for num in range(2, number + 1):
 #    product *= num
 print(product)
+
+# Test II
+# Count By
+start_num = 5
+end_num = 100
+count_by = 2
+
+break_num = start_num
+while break_num < end_num:
+    break_num += count_by
+
+print(break_num)
+
+# Count By Check
+start_num = 5
+end_num = 100
+count_by = 2
+
+if start_num > end_num:
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+
+else:
+    break_num = start_num
+    while break_num < end_num:
+        break_num += count_by
+
+    result = break_num
+
+print(result)
+
+# Nearest Square
+limit = 20
+
+num = 0
+squares = []
+while (num+1)**2 < limit:
+    num += 1
+    squares.append(num**2)
+nearest_square = num**2
+
+print(nearest_square)
+print(squares)
