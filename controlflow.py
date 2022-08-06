549b095c69458a12e1300035b847f62a998eec1b
nestaenow <nestaenow@gmail.com>
2022-08-06 15:15:26 +0100

    Check for prime numbers

--- controlflow.py
+++ controlflow.py
@@ -400,3 +400,22 @@ for headline in headlines:
         break
 
 print(news_ticker)
+
+# Test II
+# Check for prime numbers
+check_prime = [26, 39, 51, 53, 57, 79, 85]
+
+# iterate through the check_prime list
+for num in check_prime:
+
+    # search for factors, iterating through numbers ranging from 2 to the number itself
+    for i in range(2, num):
+
+        # number is not prime if modulo is 0
+        if (num % i) == 0:
+            print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
+            break
+
+# otherwise keep checking until we've searched all possible factors, and then declare it prime
+        if i == num - 1:
+            print("{} IS a prime number".format(num))
