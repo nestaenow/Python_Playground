e8c097db200105f5495f0dbaf529a0353b8b1af8
nestaenow <nestaenow@gmail.com>
2022-08-09 18:52:38 +0100

    Lambda Expressions

--- functions.py
+++ functions.py
@@ -39,3 +39,24 @@ def readable_timedelta(days):
 # test your function
 user_input = int(input())
 print(readable_timedelta(user_input))
+
+# Test III
+# Lambda with map()
+numbers = [
+    [34, 63, 88, 71, 29],
+    [90, 78, 51, 27, 45],
+    [63, 37, 85, 46, 22],
+    [51, 22, 34, 11, 18]
+]
+
+averages = list(map(lambda x: sum(x) / len(x), numbers))
+print(averages)
+
+
+# Test IV
+# Lambda with filter
+cities = ["New York City", "Los Angeles",
+          "Chicago", "Mountain View", "Denver", "Boston"]
+
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
