a5fb01fe4666429aa1961ad9dc15f998a62fea42
nestaenow <nestaenow@gmail.com>
2022-08-09 16:45:02 +0100

    Readable Time Delta

--- functions.py
+++ functions.py
@@ -16,3 +16,16 @@ print("expected result: {}, actual result: {}".format(expected_result1, test1))
 test2 = population_density(864816, 121.4)
 expected_result2 = 7123.6902801
 print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+# Test II
+# readable_timedelta
+def readable_timedelta(days):
+    # use integer division to get the number of weeks
+    weeks = days // 7
+    # use % to get the number of days that remain
+    remainder = days % 7
+    return "{} week(s) and {} day(s).".format(weeks, remainder)
+
+# test your function
+user_input = int(input())
+print(readable_timedelta(user_input))
