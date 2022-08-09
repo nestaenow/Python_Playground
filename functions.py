12600b3bf8f7ae4e39fe5d4712ba8875642c475b
nestaenow <nestaenow@gmail.com>
2022-08-09 18:04:58 +0100

    docstring under the population density function

--- functions.py
+++ functions.py
@@ -7,6 +7,16 @@ print(cylinder_volume(10, 3))
 # Test I
 # Population density function
 def population_density(population, land_area):
+    """Calculate the population density of an area.
+
+    INPUT:
+    population: int. The population of that area
+    land_area: int or float. This function is unit-agnostic, if you pass in values in terms
+    of square km or square miles the function will return a density in those units.
+
+    OUTPUT: 
+    population_density: population / land_area. The population density of a particular area.
+    """
     return population / land_area
 # test cases for your function
 test1 = population_density(10, 1)
