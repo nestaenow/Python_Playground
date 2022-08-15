267154b0352823cffad05e6be37ab15941aadc0f
nestaenow <nestaenow@gmail.com>
2022-08-15 13:58:08 +0100

    quick fix

--- script_three.py
+++ script_three.py
@@ -9,4 +9,4 @@ submit before you can graduate. You're current grade is {} and can increase \
 to {} if you submit all assignments before the due date.\n\n"
 
 for name, assignment, grade in zip(names, assignments, grades):
-    print(message.format(name, assignment, grade, int(grade) + int(assignment * 2)))
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2 ))
