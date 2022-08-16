c7592df70fe63a7748a95cada0178bfe81c2336d
nestaenow <nestaenow@gmail.com>
2022-08-16 10:32:45 +0100

    self_practice

---
+++ self_practice.py
@@ -0,0 +1,4 @@
+rec_length, rec_width = float(input("Enter the Length of your rectangle: ")), float(input("Enter the Width of your rectangle: "))
+def area_of_rec(length, width):
+    return length*width
+print(area_of_rec(rec_length, rec_width))
