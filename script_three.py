6478997e157b5677951fba1aacd53b6943bb6701
nestaenow <nestaenow@gmail.com>
2022-08-16 10:19:06 +0100

    quick fix

--- script_three.py
+++ script_three.py
@@ -1,4 +1,5 @@
-names =  input('Enter the names separated by commas: ').title().split(",")# get and process input for a list of names
+# get and process input for a list of names
+names =  input('Enter the names separated by commas: ').title().split(",")
 # get and process input for a list of the number of assignments
 assignments = input('Enter the assignment counts separated by commas: ').title().split(",")
 # get and process input for a list of grades
