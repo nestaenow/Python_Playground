ebabbd691ed388f8d0ad3c79a27da0732203e46d
nestaenow <nestaenow@gmail.com>
2022-08-19 12:24:28 +0100

    appended just the actors' names from a file (flying_circus_cast.txt) to a list (cast_list)

--- read_write.py
+++ read_write.py
@@ -8,7 +8,24 @@
 # print(file_data)
 # file_one.close
 
-files = []
-for i in range(10000):
-    files.append(open('file_one.txt', 'r'))
-    print(i)
+# files = []
+# for i in range(10000):
+#     files.append(open('file_one.txt', 'r'))
+#     print(i)
+
+def create_cast_list(filename):
+    cast_list = []
+    #use with to open the file filename
+    with open(filename) as actor_name:
+        #use the for loop syntax to process each line
+        for line in actor_name:
+            #and add the actor name to cast_list
+            cast_list.append(line.split(',')[0])
+
+    return cast_list
+
+
+cast_list = create_cast_list('flying_circus_cast.txt')
+print(cast_list)
+for actor in cast_list:
+    print(actor)
