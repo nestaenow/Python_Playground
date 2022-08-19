5b0dc2f126d144b352019e69c569243f6979886e
nestaenow <nestaenow@gmail.com>
2022-08-19 11:43:19 +0100

    read from the file we created previously

--- read_write.py
+++ read_write.py
@@ -1,3 +1,9 @@
-f = open('file_one.txt', 'w')
-f.write("Here's my first file handled in Python! Yay!")
-f.close
+# f = open('file_one.txt', 'w')
+# f.write("Here's my first file handled in Python! Yay!")
+# f.close
+
+# Now let's read the file we created above
+file_one = open('file_one.txt', 'r')
+file_data = file_one.read()
+print(file_data)
+file_one.close
