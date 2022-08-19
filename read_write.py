507b18e0844a7259a6bb26ab217cd1a271de390d
nestaenow <nestaenow@gmail.com>
2022-08-19 11:46:58 +0100

    seeing how many files can be opened before it breaks -- 8188 files

--- read_write.py
+++ read_write.py
@@ -3,7 +3,12 @@
 # f.close
 
 # Now let's read the file we created above
-file_one = open('file_one.txt', 'r')
-file_data = file_one.read()
-print(file_data)
-file_one.close
+# file_one = open('file_one.txt', 'r')
+# file_data = file_one.read()
+# print(file_data)
+# file_one.close
+
+files = []
+for i in range(10000):
+    files.append(open('file_one.txt', 'r'))
+    print(i)
