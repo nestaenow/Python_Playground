a52bd64ce45f19594384f3f8d4156590f819dd7b
nestaenow <nestaenow@gmail.com>
2022-09-14 22:47:22 +0100

    Added descriptive comment for the function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,5 @@
+# This function displays Fizz for multiples of 3,
+# Buzz for multiples of 5 and FizzBuzz for multiples of both 3 and 5
 for number in range(100):
     if number % 3 == 0 and number % 5 == 0:
         print('FizzBuzz')
