8e9547fb16ad317d2f79bb14f5bb32da7ce5a66b
nestaenow <nestaenow@gmail.com>
2022-09-26 08:42:12 +0100

    refactored "factorial.py" and created test file "test_factorial.py"

---
+++ test_factorial.py
@@ -0,0 +1,10 @@
+from factorial import factorial
+
+def test_factorial_4():
+    assert(factorial(4) == 24)
+def test_factorial_7():
+    assert(factorial(7) == 5040)
+def test_factorial_0():
+    assert(factorial(0) == 1)
+def test_factorial_5():
+    assert(factorial(5) == 120)
