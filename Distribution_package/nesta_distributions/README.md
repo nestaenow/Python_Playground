f47cdea03f1396de576a245c168e996168935f07
nestaenow <nestaenow@gmail.com>
2022-10-21 11:17:44 +0100

    created python package

---
+++ Distribution_package/nesta_distributions/README.md
@@ -0,0 +1,11 @@
+# binomial-and-guassian-distribution package
+
+Summary of the package
+
+# Files
+
+Explanation of the files in the package
+
+# Installation
+
+pip install nesta-distribution
---
+++ Distribution_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nesta_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['nesta_distributions'],
+      author='Nesta Enow',
+      author_email='nestaenow@gmail.com',
+      zip_safe=False)
